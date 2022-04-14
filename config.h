adaa54250d3395869d918e4a1bfa6922daaa3787
Frederik Bosch <frederik@bosch.dev>
2022-04-14 16:11:45 +0200

    added dwmblocks build

---
+++ config.h
@@ -0,0 +1,11 @@
+#define CMDLENGTH 60
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK("sb-disk",    1800, 19),
+	BLOCK("sb-volume",  0,    22),
+	BLOCK("sb-keyboard", 0,    23),
+	BLOCK("sb-date",    1,    24),
+	BLOCK("sb-network",    1800, 16),
+};
